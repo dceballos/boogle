757f983b186689e820d4ba3b71c6b287d9924ea7
Daniel Ceballos <daniel@cactus.fm>
2019-07-07 10:49:19 +0300

    Initial Commit

---
+++ Boggle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Boggle
+//
+//  Created by Daniel Ceballos on 7/7/19.
+//  Copyright © 2019 Boogie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Boggle/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Boggle
+//
+//  Created by Daniel Ceballos on 7/7/19.
+//  Copyright © 2019 Boogie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
